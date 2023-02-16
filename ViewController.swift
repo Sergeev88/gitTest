453364d24a1c5a4f7a7d9401cee3334a7294223c
Sergeev88 <123943804+Sergeev88@users.noreply.github.com>
2023-02-17 02:58:52 +0600

    Init
    1

---
+++ ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  gitTest
+//
+//  Created by Oleg Sergeev on 16.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("Another print statement")
+        
+    }
+
+
+}
+
